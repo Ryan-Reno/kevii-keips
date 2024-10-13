9d57f8f1ab4689b505c004aee19491546982d497
BriansTjipto <brianstjipto@gmail.com>
2024-10-14 01:23:26 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <p className="read-the-docs">KEVII GYM BOOKING</p>
+
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
