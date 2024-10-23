dcaac0ce60df32a29dea2bb942d6c95bed1203d1
BriansTjipto <brianstjipto@gmail.com>
2024-10-24 01:48:53 +0800

    Refactor capitalizeFirstLetter function to handle empty strings

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -119,5 +119,8 @@ export function getFormattedDateTime() {
 }
 
 export function capitalizeFirstLetter(string) {
+    if (!string) {
+        return '';
+    }
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
