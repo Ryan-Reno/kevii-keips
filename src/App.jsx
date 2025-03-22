0cb97d493b98ead4acdfa87fcaa45dfa750c2b0b
Ryan <ryanreno2912@gmail.com>
2025-03-22 21:24:43 +0800

    Fix routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -55,42 +55,12 @@ function App() {
         </div>
         <Router>
           <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            {/* <Route path="/login" element={<AuthRoute element={<Login />} />} />
-            <Route
-              path="/register"
-              element={<AuthRoute element={<Register />} />}
-            /> */}
+            <Route path="/" element={<Navigate to="/keips" replace />} />
             <Route
               path="/login"
               element={<AuthRoute element={<LoginPilot />} />}
             />
-            {/* <Route
-              path="/register"
-              element={<AuthRoute element={<RegisterPilot />} />}
-            /> */}
-            {/* <Route
-              path="/dashboard"
-              element={<PrivateRoute element={<Dashboard />} />}
-            />
-            <Route
-              path="/checkin"
-              element={<PrivateRoute element={<Checkin />} />}
-            />
-            <Route path="/book" element={<PrivateRoute element={<Book />} />} /> */}
             <Route path="/keips" element={<PrivateRoute element={<Keips />} />} />
-            {/* <Route
-              path="/admin"
-              element={<AdminRoute element={<AdminPage />} />}
-            />
-            <Route
-              path="/admin/qr"
-              element={<AdminRoute element={<QRCodeDisplay />} />}
-            />
-            <Route
-              path="/profile"
-              element={<PrivateRoute element={<Profile />} />}
-            /> */}
           </Routes>
         </Router>
       </div>
